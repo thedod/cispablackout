d22ceed1bd9b1f017422d5876de22544451e1168
The Dod <unclezzzen@gmail.com>
2013-04-22 06:38:31 +0700

    Changed from SOPA to CISPA
    May the force be with us.

--- README.md
+++ README.md
@@ -1,15 +1,15 @@
-SOPAstrikify.js
+cispablackout.js
 ===============
 
-This is a Javascript utility to blackout websites on January 18, 2012 from 8:00 AM to 8:00 PM EST. During that time, this utility will overlay the web page at http://sopastrike.com/strike/ on the current web page. 
-
-This code works differently from the code provided by http://sopastrike.com, which _redirects_ the user to http://sopastrike.com/strike/. Instead, this code creates an IFrame that _overlaps_ the whole web page, so that the original web page is obscured.
-
-I made this for my own web sites, but I hope others would also find it useful.
+This is a Javascript utility to blackout websites on April 22, 2013. this utility will overlay a popup linking to [sitesnotspies.org](sitesnotspies.org)
 
 License
 -------
 
-Copyright 2012 Ian Li, http://ianli.com
-
+Opriginal sopa code: Copyright 2012 Ian Li, http://ianli.com
 Licensed under [the MIT license](http://www.opensource.org/licenses/mit-license.php).
+
+The Dod mashed it up with ["Aaron's law"](http://fixthecfaa) code.
+All rights perverse.
+
+**Note:** This was done fast. If you find an error, either send a pull request, or tell me (here, or [@TheRealDod](https://twitter.com/TheRealDod)).
---
+++ cispablackout.js
@@ -0,0 +1,106 @@
+/*!
+ * cispablackout.js
+ * http://github.com/thedod/cispablackout/
+ *
+ * Forked from SOPAstrikify.js
+ * http://github.com/ianli/sopastrikify/
+ *
+ * Copyright 2012 Ian Li, http://ianli.com
+ * Licensed under the MIT license (http://www.opensource.org/licenses/mit-license.php).
+ */
+((function(){
+  var url = 'https://github.com/thedod/cispablackout/',
+      d = new Date(),
+      day = d.getDate(),
+      month = d.getMonth(),
+      year = d.getFullYear();
+  if (!(22 == day && 3 == month && 2013 == year))
+    return;
+  (function(window, document, version, callback) {
+
+    var link = document.createElement("link");
+    link.setAttribute("rel","stylesheet");
+    link.setAttribute("href",url + "modal.css");
+    var head = document.getElementsByTagName("head")[0];
+    head.appendChild(link);
+
+    var j, d;
+    var loaded = false;
+    if (!(j = window.jQuery) || version > j.fn.jquery || callback(j, loaded)) {
+        var script = document.createElement("script");
+        script.type = "text/javascript";
+        script.src = "//ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js";
+        script.onload = script.onreadystatechange = function() {
+            if (!loaded && (!(d = this.readyState) || d == "loaded" || d == "complete")) {
+                callback((j = window.jQuery).noConflict(1), loaded = true);
+                j(script).remove();
+            }
+        };
+        document.documentElement.childNodes[0].appendChild(script)
+    }
+
+  })(window, document, "1.7", function($, jquery_loaded) {
+
+    $(document).ready(function() {
+      if($(window).width() < 658) return;
+      
+      $.ajax({
+        url : url + 'modal-html.js',
+        dataType : 'jsonp',
+        jsonpCallback : 'dp',
+        success : function(response){
+          var modal  = response.html;
+          var over = $('<div>').addClass('_dp_cispa_overlay').css({
+            background : "url('"+url+"modal-over-bg.png')",
+            zIndex : 9998,
+            position : 'fixed',
+            top : 0,
+            left : 0,
+            width: '100%',
+            height: '100%'
+          }).hide().click(function(ev){
+            if($(ev.target).hasClass('_dp_cispa_overlay')){
+             $(this).remove();
+            }
+          });
+
+          var close = $('<a>').css({
+            background : "url('"+url+"close.png')",
+            width: 30,
+            height: 30,
+            position:'absolute',
+            top: -15,
+            right : -15,
+            cursor : 'pointer'
+          }).click(function(){
+            over.remove();
+          });
+
+          $('body').prepend(over.fadeIn(function(){
+            $(this).append(modal);
+            var left = parseInt(($(window).width() - $('#dp_cispa_modal').width())/2);
+            var top = 10;
+            if(($(window).height() - $('#dp_cispa_modal').height() - top) > 0){
+              top = parseInt(($(window).height() - $('#dp_cispa_modal').height())/3);
+            }
+
+            $('#dp_cispa_modal')
+              .css({
+                left : left,
+                top : top
+              })
+              .append(close)
+              .on('focus', 'input[type=text]', function(){
+                if($(this).val() === $(this).attr('placeholder')) $(this).val('');
+              })
+              .on('blur', 'input[type=text]', function(){
+                if($(this).val() === '') $(this).val($(this).attr('placeholder'));
+              });
+          }));
+        }
+      });
+  	});
+  });
+})());
+
+
