2fcf8408a63e410bea90608b27705fcf9ea7e02c
The Dod <unclezzzen@gmail.com>
2013-04-25 04:07:55 +0700

    Local banner
    Added link to senate petition

--- modal-html.js
+++ modal-html.js
@@ -1,3 +1,3 @@
 dp({
-	"html" : "<!DOCTYPE html>\n<html>\n<head>\n  <title>Cispa blackout modal window</title>\n</head>\n<body>\n  <div id=\"dp_cispa_modal\">\n    <div class=\"content\">\n<a target=\"_blank\" href=\"http://thedod.github.io/cispablackout/blackout/\"><img width=\"550\" height=\"464\" src=\"http://nt.salsalabs.com/o/501/images/CISPA%20flyer.jpg\" alt=\"Flyer for the largest online privacy protest in history.\"></a>\n    </div>\n</body>\n</html>\n"
+	"html" : "<!DOCTYPE html>\n<html>\n<head>\n  <title>Cispa blackout modal window</title>\n</head>\n<body>\n  <div id=\"dp_cispa_modal\">\n    <div class=\"content\">\n<a target=\"_blank\" href=\"http://thedod.github.io/cispablackout/blackout/\"><img width=\"550\" height=\"406\" src=\"http://thedod.github.io/cispablackout/banner.png\" alt=\"Click to help #StopCISPA\"></a>\n    </div>\n</body>\n</html>\n"
 });
